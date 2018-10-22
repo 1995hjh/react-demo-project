c57c5868c9c4381e5431b02d28cb100e3fcfc6a3
1995hjh <1634094825@qq.com>
2018-10-22 16:57:11 +0800

    feat(使用antd)：使用antd实现页面简单布局，删除测试代码

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,92 +1,33 @@
 import React, { Component, Fragment } from 'react';
-import TodoItem from './TodoItem';
-import axios from 'axios';
-import './style.css';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+
+const data = [
+	'Racing car sprays burning fuel into crowd.',
+	'Japanese princess to wed commoner.',
+	'Australian walks 100km after outback crash.',
+	'Man charged over missing wedding girl.',
+	'Los Angeles battles huge wildfires.',
+];
 
 class TodoList extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {
-			inputValue: '',
-			list: []
-		};
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleBtnClick = this.handleBtnClick.bind(this);
-		this.handleItemDelete = this.handleItemDelete.bind(this);
-	}
-
-	componentDidMount() {
-		axios.get('/api/todolist').then(
-			(res) => {
-				this.setState(() => ({
-					list: [...res.data]
-				}));
-			}
-		).catch(() => {
-			alert('error');
-		});
-	}
-
-	handleInputChange(e) {
-		const value = e.target.value;
-		this.setState(() => ({
-			inputValue: value
-		}));
-	}
-
-	handleBtnClick = () => {
-		this.setState((prevState) => ({
-			list: [...prevState.list, prevState.inputValue],
-			inputValue: ''
-		}));
-	}
-
-	handleItemDelete(index) {
-		this.setState((prevState) => {
-			let list = [...prevState.list];
-			list.splice(index, 1);
-			return {list}
-		});
-	}
-
-	getTodoItem() {
-		return this.state.list.map((item, index) => {
-			return (
-				<TodoItem
-					key={index}
-					content={item}
-					index={index}
-					deleteItem={this.handleItemDelete}/>
-			)
-		})
 	}
 
 	render() {
 		return (
-			<Fragment>
-				<div>
-					{
-						//label标签加属性htmlFor,htmlFor属性值为输入框的id，那么点击label，光标会聚焦到input标签里
-					}
-					<label htmlFor={'insertArea'}>输入内容</label>
-					<input
-						id='insertArea' 
-						className='input'
-						value={this.state.inputValue} 
-						onChange={this.handleInputChange}
-					/>
-					<button onClick={this.handleBtnClick}>提交</button>
-				</div>
-				<ul>
-					{this.getTodoItem()}
-					{/*如果希望输入的h5标签解析，这用属性dangerousSetInnerHTML*/}
-					{/*<li key={index}
-						onClick={this.handleItemDelete.bind(this, index)}
-						dangerouslySetInnerHTML={{__html: item}}>
-					</li>*/}
-				</ul>
-			</Fragment>
+			<div style={{marginTop: 10, marginLeft: 10}}>
+				<Input placeholder='todo infor' style={{width: 300, marginRight: 10}}/>
+				<Button type='primary'>提交</Button>
+				<List
+					style={{marginTop: 10, width: 300}}
+					bordered
+					dataSource={data}
+					renderItem={item => (<List.Item>{item}</List.Item>)}
+				/>
+			</div>
 		)
 	};
 }
