34e7948a9d50481252000f95b1b9311d026eb6db
1995hjh <1634094825@qq.com>
2018-11-20 13:26:56 +0800

    fix(无状态组件)：当组件没有逻辑处理，只有render的时候，修改为无状态组件，加快速度

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
 
-    render() {
-        const {
-            inputValue,
-            handleInputChange,
-            handleBtnClick,
-            list,
-            handleItemClick
-        } = this.props;
-        return (
-            <div style={{marginTop: 10, marginLeft: 10}}>
-                <Input placeholder='todo infor'
-                    value={inputValue}
-                    style={{width: 300, marginRight: 10}}
-                    onChange={handleInputChange}/>
-                <Button type='primary' onClick={handleBtnClick}>提交</Button>
-                <List
-                    style={{marginTop: 10, width: 300}}
-                    bordered
-                    dataSource={list}
-                    renderItem={(item, index) => (<List.Item onClick={(index) => {handleItemClick(index)}}>{item}</List.Item>)}
-                />
-            </div>
-        );
-    }
+const TodoListUI = (props) => {
+     const {
+        inputValue,
+        handleInputChange,
+        handleBtnClick,
+        list,
+        handleItemClick
+    } = props;
+
+    return (
+        <div style={{marginTop: 10, marginLeft: 10}}>
+            <Input placeholder='todo infor'
+                value={inputValue}
+                style={{width: 300, marginRight: 10}}
+                onChange={handleInputChange}/>
+            <Button type='primary' onClick={handleBtnClick}>提交</Button>
+            <List
+                style={{marginTop: 10, width: 300}}
+                bordered
+                dataSource={list}
+                renderItem={(item, index) => (<List.Item onClick={(index) => {handleItemClick(index)}}>{item}</List.Item>)}
+            />
+        </div>
+    );
 }
 
 export default TodoListUI;
